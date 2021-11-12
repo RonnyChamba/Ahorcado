bdbe6d3eb62d07d21d22756f806c1d0a4d4f0a0a
Ronny <ronnychamba96@gmail.com>
2021-11-12 16:55:44 -0500

    init-components

--- src/main/webapp/js/script_juego.js
+++ src/main/webapp/js/script_juego.js
@@ -8,14 +8,65 @@ import {
 	createWraperDetalleJuego, $modal
 } from "./script_modal.js";
 
+import  {initComponents } from "./script_juego_jugar.js"; 
+
 const $botonesJugar = document.querySelector(".jugar__opcion");
 const $botonesLetra = document.querySelector(".jugar__item--letras");
 const $verAyuda = document.querySelector(".aside-menu__item");
 const $modalContentSelect = createWraperCategoria();
 const $modalContentResultado = createWraperResultado();
 const $modalContentDetalleJuego = createWraperDetalleJuego();
-const labelTimer = document.getElementById("time-js");
+const $labelTimer = document.getElementById("time-js");
 const serlvet = "CJuego";
+
+
+const datosJuego = {
+	
+	partida: Partida,
+	modal :$modal,
+	modalContentSelect: $modalContentSelect,
+	botonesJugar: $botonesJugar,
+	botonesLetra : $botonesLetra,
+	modalContentResultado : $modalContentResultado,
+	verAyuda : $verAyuda,
+	modalContentDetalleJuego: $modalContentDetalleJuego,
+	labelTimer : $labelTimer,
+	servelt: serlvet,
+	createQueryString : createQueryString,
+	ajax: ajax,
+	createWraperMensaje: createWraperMensaje,
+	changeTextBtnTitle:changeTextBtnTitle,
+	showAlert:showAlert	
+}
+
+
+initComponents (datosJuego);
+
+
+/*
+
+partida, 
+modal,
+modalContentSelect, 
+botonesJugar, 
+botonesLetra,
+modalContentResultado, 
+verAyuda,
+modalContentDetalleJuego,
+labelTimer,
+controlServlet,
+fnQueryString,
+fnAjax,
+fnCreateWraperMensaje,
+fnChangeTextBtnTitle,
+fnShowAlert
+*/
+
+
+
+
+
+/*
 
 let miPartida = null
 let timerPartida = null;
@@ -261,10 +312,10 @@ function datosOk() {
 }
 
 function setOptionsAyuda(tipoAccion = true) {
-	/* 
-	true: nuevo juego
-	false: resetear todo
-	 */
+ 
+	//true: nuevo juego
+	//false: resetear todo
+	
 	const $helpOptions = $verAyuda.querySelectorAll(".row .help-juego");
 	$helpOptions.forEach((elemento) => {
 
@@ -312,10 +363,10 @@ function cancelPartida() {
 
 function setPalabraImgJuego(tipoAccion = true) {
 
-	/* 
-	true: nuevo juego
-	false: resetear
-	 */
+	
+	//true: nuevo juego
+	//false: resetear
+	
 	const $palabraJuego = document.getElementById("palabra-juego-main");
 	$palabraJuego.textContent = tipoAccion ? miPartida.mostrarTextoPalabra() : "COMIENZA A JUGAR";
 
@@ -456,5 +507,6 @@ function timerJuego() {
 	}, 1000);
 }
 
+*/
 
-console.log("LEIDO JUEGO")
+console.log("LEIDO JUEGO SCRIP_JUEGO.js")
--- src/main/webapp/js/script_juego_jugar.js
+++ src/main/webapp/js/script_juego_jugar.js
@@ -1,32 +1,44 @@
+console.log("Inicio de juego_jugar");
+
 // Definición de variables globales
 let miPartida = null
 let timerPartida = null;
 let $botonesJugar = null;
 let $botonesLetra = null;
-let ObjPartida = null;
+let Partida = null;
 let $modal = null;
 let $modalContentSelect = null;
 let $modalContentResultado = null;
+let $modalContentDetalleJuego = null;
+let $verAyuda = null;
+let $labelTimer = null;
 let changeTextBtnTitle = null;
 let createQueryString = null;
 let createWraperMensaje = null;
+let showAlert = null;
 let ajax = null; 
+let serlvet = null;
 
-export function initComponents(partida, modal, modalContentSelect, 
-								botonesJugar, botonesLetra,
-								modalContentResultado,
-								fnQueryString, fnAjax, fnCreateWraperMensaje,
-								fnChangeTextBtnTitle){
-	ObjPartida = partida;
-	$modal = modal;
-	$modalContentSelect = modalContentSelect;
-	$botonesJugar = botonesJugar;
-	$botonesLetra = botonesLetra;
-	$modalContentResultado = modalContentResultado;
-	createQueryString = fnQueryString;
-	ajax = fnAjax;
-	createWraperMensaje = fnCreateWraperMensaje;
-	changeTextBtnTitle = fnChangeTextBtnTitle;	
+export function initComponents(datosJuego){
+	Partida = datosJuego.partida;
+	$modal = datosJuego.modal;
+	$modalContentSelect = datosJuego.modalContentSelect;
+	$botonesJugar = datosJuego.botonesJugar;
+	$botonesLetra = datosJuego.botonesLetra;
+	$modalContentResultado = datosJuego.modalContentResultado;
+	$modalContentDetalleJuego = datosJuego.modalContentDetalleJuego;
+	$verAyuda = datosJuego.verAyuda;
+	$labelTimer = datosJuego.labelTimer;
+	createQueryString = datosJuego.createQueryString;
+	ajax = datosJuego.ajax;
+	serlvet = datosJuego.servlet;
+	showAlert = datosJuego.showAlert;
+	createWraperMensaje = datosJuego.createWraperMensaje;
+	changeTextBtnTitle = datosJuego.changeTextBtnTitle;	
+
+	console.log($modal);
+	console.log($modalContentSelect);
+	
 }
 
 // Fn recibe el  parent de los botones  iniciar, cancelar, info
@@ -38,12 +50,12 @@ function parentBotonesPartida(){
 
 // Fn recibe el parent de las letras del juego
 function parentBotonesLetras(){
-	$botonesLetra.addEventListener("click", (event) => clickBtnLetras(event)); // SN
+	$botonesLetra.addEventListener("click", (event) => clickBtnLetras(event));
 }
 
 // Fn recibe el parent modal
 function parentModal(){
-	$modal.addEventListener("click", (event) => preIniciarJuego(event)); // SN
+	$modal.addEventListener("click", (event) => preIniciarJuego(event)); 
 }
 
 // Fn maneja los botones iniciar,detener, info de una partida 
@@ -178,3 +190,295 @@ function dataFinPartidaJuego() {
 }
 
 
+function clickBtnLetras(event) {
+
+	if (miPartida !== null) {
+
+		let typeElement = event.target;
+		let letraPresionada = typeElement.textContent;
+		// Verificar si es button  y ademas que si ya fue presionada o no anteriormente
+		if (typeElement.matches("button") && !miPartida.getLetrasPulsadas.includes(letraPresionada)) {
+
+			// Cambiar el tipo de cursor para el elemento presionado
+			typeElement.classList.add("btn-cursor-no-pointer");
+			let isContentLetra = miPartida.verificarLetraContenida(letraPresionada);
+			setPalabraImgJuego(true);
+
+			if (miPartida.isFinJuego) {
+				clearInterval(timerPartida); // detener el timer
+				showModalFinPartida();
+			}
+		}
+		return;
+	}
+
+	showMensajesNotificacion("No existe una partida para jugar, crea una nueva ..."); // SN
+}
+
+
+function showModalFinPartida() {
+
+	// Guardar en la base de datos
+	aceptarFinPartida();
+	$modalContentResultado.querySelector("#modal-img-resultado").setAttribute("src", miPartida.getPathImg);
+	$modalContentResultado.querySelector(".modal__mensaje").textContent = miPartida.getSmsFinJuego;
+	$modal.querySelector("#modal-btn-aceptar").dataset["action"] = "modal-btn-aceptar-fin-juego";
+	$modal.querySelector("#modal-btn-cancelar").dataset["action"] = "modal-btn-detalles-fin-juego";
+	selectContentModal("fin-juego");
+
+}
+
+// Cuando se gane o pierda
+function aceptarFinPartida() {
+	let idPalabra = miPartida.getPalabra.id;
+	let puntaje = miPartida.getPuntos;
+	let tiempo = 30;
+	let fechaJuego = miPartida.getFechaJuego;
+	const datos = {
+		action: "guardarJuego",
+		idPalabra,
+		puntaje,
+		tiempo,
+		fechaJuego
+	}
+
+	guardarJuegoBD(datos);
+}
+
+
+async function guardarJuegoBD(datos) {
+
+	try {
+		let queryString = createQueryString(datos);
+		let xhrGuadarJuego = await ajax(serlvet, queryString);
+		let responseJuego = xhrGuadarJuego.xhr.responseText;
+		responseJuego = JSON.parse(responseJuego);
+		showMensajesNotificacion(responseJuego.sms, responseJuego.estado ? "succes" : "warning");
+
+
+
+	} catch (error) {
+		// Error de ajax o error de este bloque
+		console.log("Error en guarsar Juego ", error);
+	} finally {
+
+	}
+}
+
+function preIniciarJuego(event) {
+	let element = event.target;
+	if (element.matches("button")) {
+		let buttonAction = element.dataset["action"];
+		// console.log("Data Action : ", element.dataset);
+
+		// Nuevo Juego
+		if (buttonAction === "modal-btn-aceptar" && miPartida === null) getOptionSeleccionada();
+
+		// Seguir Jugando
+		if (buttonAction === "modal-btn-aceptar" && miPartida !== null) $modal.classList.remove("modal--show");
+
+		// Opcion Cancelada nuevo Juego
+		if (buttonAction === "modal-btn-cancelar" && miPartida === null) $modal.classList.remove("modal--show");
+
+		// Cancelar Partida en Juego
+		if (buttonAction === "modal-btn-cancelar" && miPartida !== null) cancelPartida();
+
+		// Finaliza el juego(pierda o gane) y acepta 
+		if (buttonAction === "modal-btn-aceptar-fin-juego") cancelPartida();
+
+
+		// Detalles del juego despues de finalizar 
+		if (buttonAction === "modal-btn-detalles-fin-juego") detallesJuego();
+	}
+
+}
+
+function getOptionSeleccionada() {
+	let $optionSelect = $modal.querySelector("#modal-categoria");
+	let index = $optionSelect.selectedIndex;
+	if (index === -1) {
+		showMensajesNotificacion("Tiene que seleccionar una categoria");
+		return;
+	}
+
+	let idCate = $optionSelect.options[index].value;
+	getDatosPalabra(idCate);
+}
+
+async function getDatosPalabra(idCategoria) {
+	const datos = { action: "datosPalabra", idCategoria }
+	let queryString = createQueryString(datos);
+	try {
+
+		let xhrPalabra = await ajax(serlvet, queryString);
+		let responsePalabra = xhrPalabra.xhr.responseText;
+		responsePalabra = JSON.parse(responsePalabra);
+
+		if (responsePalabra.estado) {
+			// Pintar los datos para jugar
+			setDatosPartida(responsePalabra.palabra);
+			// quitar el load de las letras, add una clase para ocultarlo
+			document.querySelector(".jugar__item--letras-load").classList.add("jugar__item--letras-load--hide");
+
+			// Iniciar el tiempo	
+			timerJuego(); // SN
+
+			return;
+		}
+
+		showMensajesNotificacion(responsePalabra.sms);
+	} catch (error) {
+
+	}
+
+}
+
+async function setDatosPartida(dataPalabra) {
+
+	let objPalabra = JSON.parse(dataPalabra);
+	miPartida = new Partida(objPalabra);
+
+	try {
+
+		if (await datosOk()) $modal.classList.remove("modal--show");
+	} catch (error) {
+
+	}
+}
+
+function datosOk() {
+
+	const miPromise = new Promise((resolve, reject) => {
+		// Asignar los datos de ayuda(aside)
+		setOptionsAyuda(true);
+		setPalabraImgJuego(true);
+		resolve({ estado: true });
+	});
+	return miPromise;
+}
+
+function timerJuego() {
+	let contador = 0;
+	timerPartida = setInterval(() => {
+		contador++;
+		miPartida.setSegundosJuego = contador;
+		$labelTimer.textContent = miPartida.getSegundosJuego;
+		if (miPartida.getSegundosJuego === 60) {
+			clearInterval(timerPartida); // Detener el timer
+			showModalFinPartida();
+		}
+	}, 1000);
+}
+
+
+function cancelPartida() {
+	setOptionsAyuda(false);
+	setPalabraImgJuego(false);
+	// Remover o add clases de los elementos para que vuelva a sus estado original
+
+	// ubicar nuevamente load de las letras, eliminar clase para que aparesca
+	document.querySelector(".jugar__item--letras-load").classList.remove("jugar__item--letras-load--hide");
+	
+	// Botones del modal Aceptar y Cancelar
+	$modal.querySelector("#modal-btn-aceptar").dataset["action"] = "modal-btn-aceptar";
+	// Se oculto previamente cuando mostre los detalles del juego
+	$modal.querySelector("#modal-btn-cancelar").style.display = "block";
+	$modal.querySelector("#modal-btn-cancelar").dataset["action"] = "modal-btn-cancelar";
+	
+	// Volver el estado del curso de los botones de las letras(xq cuando son pulsados cambiar de tipo cursor)
+	[...$botonesLetra.querySelectorAll("button.btn-cursor-no-pointer")].forEach((element) => {
+		element.classList.remove("btn-cursor-no-pointer");
+
+	});	
+	miPartida = null;
+	clearInterval(timerPartida);
+	timerPartida = null;
+	$labelTimer.textContent = "00";
+	
+	$modal.classList.remove("modal--show");
+
+}
+
+function setOptionsAyuda(tipoAccion = true) {
+	/* 
+	true: nuevo juego
+	false: resetear todo
+	 */
+	const $helpOptions = $verAyuda.querySelectorAll(".row .help-juego");
+	$helpOptions.forEach((elemento) => {
+
+		if (elemento.classList.contains("help-categoria")) {
+			elemento.textContent = tipoAccion ? miPartida.getCategoria.name : "";
+		}
+		if (elemento.classList.contains("help-desc-categoria")) {
+			elemento.textContent = tipoAccion ? miPartida.getCategoria.descripcion : "";
+		}
+		if (elemento.classList.contains("help-desc-palabra")) {
+			elemento.textContent = tipoAccion ? `${miPartida.getPalabra.descripcion}` : "";
+		}
+	});
+}
+
+function setPalabraImgJuego(tipoAccion = true) {
+
+	/* 
+	true: nuevo juego
+	false: resetear
+	 */
+	const $palabraJuego = document.getElementById("palabra-juego-main");
+	$palabraJuego.textContent = tipoAccion ? miPartida.mostrarTextoPalabra() : "COMIENZA A JUGAR";
+
+	const $imgJuego = document.getElementById("img-juego-main");
+	$imgJuego.setAttribute("src", tipoAccion ? miPartida.getPathImg : "img/img-estado/ahor0.png");
+}
+
+
+function detallesJuego() {
+	// Cambiar el action de los botones del modal
+	$modal.querySelector("#modal-btn-aceptar").dataset["action"] = "modal-btn-aceptar-fin-juego";
+	// Oculto el boton
+	$modal.querySelector("#modal-btn-cancelar").style.display = "none";
+	let fragment = document.createDocumentFragment();
+
+	const objDetalle = {
+		"Palabra": miPartida.getPalabra.name,
+		"Descripción": miPartida.getPalabra.descripcion,
+		"Categoria": miPartida.getCategoria.name,
+		"Letras Pulsadas": miPartida.getLetrasPulsadas,
+		"Palabra en Juego": miPartida.mostrarTextoPalabra(),
+		"Segundos": miPartida.getSegundosJuego
+	}
+
+	for (let key in objDetalle) {
+
+		let opcionDetalle = document.createElement("P");
+		opcionDetalle.innerHTML = `
+	<span class ="detalle-juego__option detalle-juego__option--key"> ${key}: </span> : <span class ="detalle-juego__option detalle-juego__option--value"> ${objDetalle[key]} </span> `;
+		fragment.appendChild(opcionDetalle);
+	}
+
+	$modalContentDetalleJuego.querySelector("#content-detalle-js").innerHTML = "";
+	$modalContentDetalleJuego.querySelector("#content-detalle-js").appendChild(fragment);
+
+	// Wraper para del nuevo contenido  modal
+	let $modalContent = $modal.querySelector(".modal__item-content");
+	// Limpiar contenido
+	$modalContent.innerHTML = "";
+	// Add contenido nuevo en la seccion contenido del modal
+	$modalContent.insertAdjacentElement("afterbegin", $modalContentDetalleJuego);
+}
+
+function showMensajesNotificacion(mensaje = "Mensaje Sin Especificar", estado = "warning") {
+
+	const data = {
+		sms1: {
+			mensaje,
+			estado
+		}
+	};
+	const $alert = document.getElementById("alert");
+	showAlert($alert, data);
+}
+
+
+console.log("Fin de juego_jugar");
